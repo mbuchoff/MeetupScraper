9adeea57411e934bd98b2a91239c071b8c295102
Mike <mbuchoff@gmail.com>
2016-01-03 01:29:53 -0500

    Now creates zip files for each event.

--- MeetupScraper/MainWindow.cs
+++ MeetupScraper/MainWindow.cs
@@ -1,15 +1,17 @@
 using System;
-using Gtk;
-using System.Net;
-using System.IO;
-using System.Security.Cryptography.X509Certificates;
-using System.Net.Security;
 using System.Collections.Generic;
-using Newtonsoft.Json;
-using System.Text;
 using System.Globalization;
-using System.Threading;
+using System.IO;
 using System.IO.Compression;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using Gtk;
+using Newtonsoft.Json;
+using ICSharpCode.SharpZipLib.Zip;
+using SystemPath = System.IO.Path;
 
 public class MeetupGroup
 {
@@ -252,6 +254,10 @@ public partial class MainWindow: Gtk.Window
 							DateTime epoch = new DateTime (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 							DateTime eventTime = epoch.AddSeconds (seconds);
 
+							string meetupEventName = meetupEvent.name.Replace ('/', '_').Replace ('\\', '_');
+							string eventTimeString = eventTime.ToString ("yyyy-MM-dd h:mm tt", CultureInfo.InvariantCulture);
+							string directory = "/home/mbuchoff/Pictures/" + eventTimeString + " - " + meetupEventName;
+
 							if (continueDownloading)
 							{
 								JsonPhotoAlbums photoAlbums = JsonConvert.DeserializeObject<JsonPhotoAlbums> (getJsonFromUrl ("https://api.meetup.com/2/photo_albums?&sign=true&photo-host=public&group_id=" + meetupGroup.id + "&event_id=" + meetupEvent.id + "&page=20"));
@@ -267,11 +273,6 @@ public partial class MainWindow: Gtk.Window
 
 										foreach (JsonMeetupPhoto photo in photos)
 										{
-											string meetupEventName = meetupEvent.name.Replace ('/', '_').Replace ('\\', '_');
-
-											string eventTimeString = eventTime.ToString ("yyyy-MM-dd h:mm tt", CultureInfo.InvariantCulture);
-											string directory = "/home/mbuchoff/Pictures/" + eventTimeString + " - " + meetupEventName;
-
 											if (!createdDirectory)
 											{
 												Directory.CreateDirectory (directory);
@@ -281,19 +282,72 @@ public partial class MainWindow: Gtk.Window
 											if (continueDownloading)
 											{
 												client.DownloadFile (photo.highres_link, directory + "/" + photo.id + ".jpeg");
-												using (ZipArchive zipArchive = new ZipArchive())
-												{
-
-												}
 											}
 										}
 									}
+								}
+
+								if (createdDirectory)
+								{
+									CompressDirectory (directory, directory + ".zip");
 								}
 							}
 						}
 					}
 				}
 			}
+		}
+	}
+
+	void CompressDirectory(string directoryInput, string zipOutput)
+	{
+		// Depending on the directory this could be very large and would require more attention
+		// in a commercial package.
+		string[] filenames = Directory.GetFiles(directoryInput);
+
+		// 'using' statements guarantee the stream is closed properly which is a big source
+		// of problems otherwise.  Its exception safe as well which is great.
+		using (ZipOutputStream s = new ZipOutputStream(File.Create(zipOutput))) {
+
+			s.SetLevel(9); // 0 - store only to 9 - means best compression
+
+			byte[] buffer = new byte[4096];
+
+			foreach (string file in filenames) {
+
+				// Using GetFileName makes the result compatible with XP
+				// as the resulting path is not absolute.
+				ZipEntry entry = new ZipEntry(SystemPath.GetFileName(file));
+
+				// Setup the entry data as required.
+
+				// Crc and size are handled by the library for seakable streams
+				// so no need to do them here.
+
+				// Could also use the last write time or similar for the file.
+				entry.DateTime = DateTime.Now;
+				s.PutNextEntry(entry);
+
+				using ( FileStream fs = File.OpenRead(file) ) {
+
+					// Using a fixed size buffer here makes no noticeable difference for output
+					// but keeps a lid on memory usage.
+					int sourceBytes;
+					do {
+						sourceBytes = fs.Read(buffer, 0, buffer.Length);
+						s.Write(buffer, 0, sourceBytes);
+					} while ( sourceBytes > 0 );
+				}
+			}
+
+			// Finish/Close arent needed strictly as the using statement does this automatically
+
+			// Finish is important to ensure trailing information for a Zip file is appended.  Without this
+			// the created file would be invalid.
+			s.Finish();
+
+			// Close is important to wrap things up and unlock the file.
+			s.Close();
 		}
 	}
 
